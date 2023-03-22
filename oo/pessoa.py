243dd0fdbe6d6e4200db941da7e2e7e2ab2f8d81
gabriel <prof.gabrielsantana@gmail.com>
2023-03-22 17:40:32 -0300

    Criado atributo de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,25 @@
 class Pessoa:
+    def __init__(self, nome= None):
+        """
+        Atributo de instaância :  Atributos usados para criação de um objeto.
+        """
+        self.nome = nome
     def cumprimentar(self):
         return  f'Como vai você?{id(self)}'
 
 if __name__ == '__main__':
-       p = Pessoa()
+       p = Pessoa('Vascaino')
        print(Pessoa.cumprimentar(p))
        print("-"*100)
        print(id(p))
        print("-" * 100)
        print( p.cumprimentar())
        print("-" * 100)
+       print("ATRIBUTO DE INSTÂNCIA")
+       print(f"p.nome :  {p.nome}")
+       print("-" * 100)
+       p.nome = 'Gabriel Santana'
+       print(f"p.nome :  {p.nome}")
+
 
 
