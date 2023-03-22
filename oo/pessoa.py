64364349b6af9bcd992688c4ff7dcd18fdedb936
gabriel <prof.gabrielsantana@gmail.com>
2023-03-22 17:06:40 -0300

    Criada a classe Pessoa(). Criado metodo cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,14 @@
 class Pessoa:
-    d = '1234'
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return  f'Como vai você?{id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+       p = Pessoa()
+       print(Pessoa.cumprimentar(p))
+       print("-"*100)
+       print(id(p))
+       print("-" * 100)
+       print( p.cumprimentar())
+       print("-" * 100)
+
 
